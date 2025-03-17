59e421684d8cd78939734f52e5dce22883171b29
Winston DeCredico <winstondecredico@gmail.com>
2025-03-17 05:01:46 -0600

    Added y dimension diffusion equation

--- equations.py
+++ equations.py
@@ -1,32 +1,37 @@
-import numpy as np
-from constants import Constants
-from grid2d import Grid
 from abc import ABC, abstractmethod
 
+import numpy as np
+
+from constants import Constants
+from grid2d import Grid
+
+
 class DyDtEquation(ABC):
+    """Parent class for first order differential equation of form \frac{dy}{dt} = f(t, y)"""
 
     @abstractmethod
     def __call__(self, t, y):
         pass
+
 
 class GreybodyConservationOfEnergyEquation(DyDtEquation):
 
     def __init__(self, final_time):
         self.final_time = final_time
 
-    STEFAN_BOLTZMANN_CONSTANT = 5.67E-8 #units of W/(m^2 * K^4)
+    STEFAN_BOLTZMANN_CONSTANT = 5.67E-8  # units of W/(m^2 * K^4)
     SIGMA = STEFAN_BOLTZMANN_CONSTANT
 
-    DEFAULT_SOLAR_CONSTANT = 1361  #units of W/m^2
+    DEFAULT_SOLAR_CONSTANT = 1361  # units of W/m^2
     S_0 = DEFAULT_SOLAR_CONSTANT
 
-    PLANET_RADIUS = 6.371E6 #units of meters
+    PLANET_RADIUS = 6.371E6  # units of meters
     R = PLANET_RADIUS
 
     EMISSIVITY = .78
     EPSILON = EMISSIVITY
 
-    SILICON_HEAT_CAPACITY = 712 #units of J/(kg*K)
+    SILICON_HEAT_CAPACITY = 712  # units of J/(kg*K)
     C_S = SILICON_HEAT_CAPACITY
 
     SILICION_DENSITY = 2650  # units of kg/m^3
@@ -35,14 +40,13 @@ class GreybodyConservationOfEnergyEquation(DyDtEquation):
     PLANET_ALBEDO = .3
     ALPHA = PLANET_ALBEDO
 
-    INITIAL_TEMPERATURE = 263.5  #units of kelvins
+    INITIAL_TEMPERATURE = 263.5  # units of kelvins
     T_0 = INITIAL_TEMPERATURE
 
     PLANET_HEAT_CAPACITY = 2.0437948107045676e27
     C_E = PLANET_HEAT_CAPACITY
 
     def calculate_solar_constant(self, t):
-
         rise = .3 * self.S_0
         run = self.final_time
         slope = rise / run
@@ -52,74 +56,58 @@ class GreybodyConservationOfEnergyEquation(DyDtEquation):
         return solar_constant
 
     def __call__(self, t, T):
-
         solar_constant = self.calculate_solar_constant(t)
 
+        dTdt = (((np.pi * (self.R ** 2)) / self.C_E) *
+                (solar_constant * (1 - self.ALPHA) - ((1 - (self.EPSILON / 2)) * 4 * self.SIGMA * (T ** 4))))
 
-        dTdt = ((np.pi*(self.R**2))/self.C_E) * (solar_constant*(1-self.ALPHA) - ((1-(self.EPSILON/2))*4*self.SIGMA*(T**4)))
-
-        #returns in units of kelvin/second
+        # returns in units of kelvin/second
         return dTdt
+
 
 class XDirectionDiffusion(DyDtEquation):
 
     def __init__(self, diffusion_constant: float, grid: Grid):
-
         self.diffusion_constant = diffusion_constant
         self.grid = grid
 
-    def __call__(self, time, temperature):
+    def __call__(self, time: float, temperature: np.ndarray) -> np.ndarray:
+        """Calculates temperature diffusion on the x-axis.  Returns a 2d numpy array
+        of diffusion in units of [units here] for each x interface position"""
 
         # temperature change across cells in the x direction
-        dt_dx = np.zeros(self.grid.x_intersection_points.shape)
         dt_dx = (self.grid.temperatures[1:-1, 1:] - self.grid.temperatures[1:-1, :-1]) / self.grid.x_segment_length
 
-        x_direction_flux = -1 * self.diffusion_constant * dt_dx
+        x_direction_flux: np.ndarray = -1 * self.diffusion_constant * dt_dx
 
         incoming_shortwave_radiation = Constants.SOLAR_IRRADIANCE / 4 * (1 - Constants.EARTH_ALBEDO)
-        outgoing_longwave_radiation = (1 - Constants.EMISSIVITY / 2) * Constants.STEFAN_BOLTZMANN_CONSTANT * temperature**4
-        # x_direction_diffusion = -1 * x_direction_flux[]
+        outgoing_longwave_radiation = (1 - Constants.EMISSIVITY / 2) * Constants.STEFAN_BOLTZMANN_CONSTANT * temperature ** 4
+        x_direction_flux_term = (x_direction_flux[:, 1:] - x_direction_flux[:, :-1]) / self.grid.x_segment_length
 
-        return
+        x_direction_diffusion = (incoming_shortwave_radiation - outgoing_longwave_radiation - x_direction_flux_term) / self.grid.heat_capacity
 
-class DiffusionTemperatureChange(DyDtEquation):
+        return x_direction_diffusion
+
+
+class YDirectionDiffusion(DyDtEquation):
 
     def __init__(self, diffusion_constant: float, grid: Grid):
-
         self.diffusion_constant = diffusion_constant
         self.grid = grid
 
-    def solar_power(self, y):
+    def __call__(self, time: float, temperature: np.ndarray) -> np.ndarray:
+        """Calculates temperature diffusion on the y-axis.  Returns a 2d numpy array
+            of diffusion in units of [units here] for each y interface position"""
 
-        #figure out this later
-        something = 4
-        return something
+        # temperature change across cells in the y direction
+        dt_dy = (self.grid.temperatures[1:, 1:-1] - self.grid.temperatures[:-1, 1:-1]) / self.grid.y_segment_length
 
-    def __call__(self, time, temperature) -> np.ndarray:
+        y_direction_flux: np.ndarray = -1 * self.diffusion_constant * dt_dy
 
+        incoming_shortwave_radiation = Constants.SOLAR_IRRADIANCE / 4 * (1 - Constants.EARTH_ALBEDO)
+        outgoing_longwave_radiation = (1 - Constants.EMISSIVITY / 2) * Constants.STEFAN_BOLTZMANN_CONSTANT * temperature ** 4
+        y_direction_flux_term = (y_direction_flux[1:, :] - y_direction_flux[:-1, :]) / self.grid.y_segment_length
 
-        #temperature change across cells in the x direction
-        dt_dx = np.zeros(self.grid.x_intersection_points.shape)
-        dt_dx = (self.grid.temperatures[1:-1, 2:]  - self.grid.temperatures[1:-1, :-2]) / self.grid.x_segment_length
+        y_direction_diffusion = (incoming_shortwave_radiation - outgoing_longwave_radiation - y_direction_flux_term) / self.grid.heat_capacity
 
-
-
-        #temperature change across cells in the y direction
-        dt_dy = np.zeros(self.grid.y_intersection_points.shape)
-        dt_dy[:] = (self.grid.temperatures[1:, :] - self.grid.temperatures[0:-1, :]) / self.grid.y_segment_length
-
-        x_direction_flux = -1 * self.diffusion_constant * dt_dx
-        y_direction_flux = -1 * self.diffusion_constant * dt_dy
-
-
-
-        incoming_shortwave_radiation = Constants.SOLAR_IRRADIANCE/4 * (1-Constants.EARTH_ALBEDO)
-        outgoing_longwave_radiation = (1 - Constants.EMISSIVITY/2) * Constants.STEFAN_BOLTZMANN_CONSTANT * temperature**4
-        #x_direction_diffusion = -1 * x_direction_flux[]
-
-        return
-
-
-
-
-
+        return y_direction_diffusion
--- grid2d.py
+++ grid2d.py
@@ -1,15 +1,13 @@
-from constants import Constants
 import numpy as np
-from scipy.integrate import solve_ivp
+
+
 
 class Grid:
 
-
     def __init__(self, x_dimension: int, y_dimension: int):
 
-        self.x_dimension = x_dimension
+        self.x_dimension: int = x_dimension
         self.y_dimension = y_dimension
-
 
         self.midpoints = self.create_grid_midpoints()
         self.x_intersection_points, self.y_intersection_points = self.create_grid_interface_points()
@@ -17,8 +15,10 @@ class Grid:
         self.x_segment_length = self.midpoints[0, 1] - self.midpoints[0, 0]
         self.y_segment_length = self.midpoints[1, 0] - self.midpoints[0, 0]
 
-        self.temperatures = np.zeros((y_dimension+2, x_dimension+2))
-        self.boundary_condition = None
+        self.temperatures = np.zeros((y_dimension + 2, x_dimension + 2))
+        self.boundary_condition: str = ""
+
+        self.heat_capacity = np.zeros((y_dimension, x_dimension))
 
     def create_grid_midpoints(self) -> np.ndarray:
         """Creates a grid storing coordinate points.  Includes coordinate points of the boundary."""
@@ -34,47 +34,41 @@ class Grid:
 
         return grid_midpoints
 
-
     def create_grid_interface_points(self) -> tuple[np.ndarray, np.ndarray]:
         """Creates two 2d numpy arrays.  One for coordinates of grid interface points on the x-axis,
             and one for grid interface points on the y-axis."""
 
-
         def create_x_interfaces() -> np.ndarray:
 
-            grid_x_interfaces = np.zeros((self.y_dimension, self.x_dimension+1, 2))
+            grid_x_interfaces = np.zeros((self.y_dimension, self.x_dimension + 1, 2))
 
-            for x_index in range(self.x_dimension+1):
-                grid_x_interfaces[:, x_index, 0] = x_index - (self.x_dimension/2)
+            for x_index in range(self.x_dimension + 1):
+                grid_x_interfaces[:, x_index, 0] = x_index - (self.x_dimension / 2)
 
                 for y_index in range(self.y_dimension):
                     grid_x_interfaces[y_index, x_index, 1] = -1 * (y_index - (self.y_dimension - 1) / 2)
-
 
             return grid_x_interfaces
 
         def create_y_interfaces() -> np.ndarray:
 
-            grid_y_interfaces = np.zeros((self.y_dimension+1, self.x_dimension, 2))
+            grid_y_interfaces = np.zeros((self.y_dimension + 1, self.x_dimension, 2))
 
             for x_index in range(self.x_dimension):
-                grid_y_interfaces[:, x_index, 0] = x_index - (self.x_dimension - 1)/2
+                grid_y_interfaces[:, x_index, 0] = x_index - (self.x_dimension - 1) / 2
 
-                for y_index in range(self.y_dimension+1):
-                    grid_y_interfaces[y_index, x_index, 1] = -1 * (y_index - (self.y_dimension/2))
-
+                for y_index in range(self.y_dimension + 1):
+                    grid_y_interfaces[y_index, x_index, 1] = -1 * (y_index - (self.y_dimension / 2))
 
             return grid_y_interfaces
 
-
         return create_x_interfaces(), create_y_interfaces()
-
 
     def set_initial_temperatures(self, temperatures: np.ndarray) -> None:
         """Sets initial temperature based on a 2d numpy array passed in.
             temperatures must be a 2d numpy array with size [y_dimension, x_dimension]"""
 
-        #check that input is the right size
+        # check that input is the right size
         if not temperatures.shape == (self.y_dimension, self.x_dimension):
             print("Input of initial temperatures is not the correct shape.")
 
@@ -84,17 +78,14 @@ class Grid:
         """Sets the boundary conditions. Input must be a 2d numpy array
             with size (y_dimension+2, x_dimension+2)"""
 
-
-        #check that the input is the right size
-        if not boundary.shape == (self.y_dimension+2, self.x_dimension+2):
+        # check that the input is the right size
+        if not boundary.shape == (self.y_dimension + 2, self.x_dimension + 2):
             print("Input of boundary is not the correct shape.")
-
 
         self.temperatures[:, 0] = boundary[:, 0]
         self.temperatures[:, -1] = boundary[:, -1]
         self.temperatures[0, :] = boundary[0, :]
         self.temperatures[-1, :] = boundary[-1, :]
-
 
     def set_boundary_condition(self, boundary_condition: str) -> None:
 
@@ -117,8 +108,14 @@ class Grid:
     def get_boundary_condition(self) -> str:
         return self.boundary_condition
 
+    def set_heat_capacity(self, heat_capacity: np.ndarray) -> None:
+        """Takes in a 2d numpy array for what the heat capacity is at each cell."""
 
+        # check that input is the right size
+        if not heat_capacity.shape == (self.y_dimension, self.x_dimension):
+            print(f"Heat Capacity array is the wrong size.")
+            print(f"Grid dimension: ({self.y_dimension}, {self.x_dimension})")
+            print(f"Heat capacity array: {heat_capacity.shape}")
+            quit()
 
-
-
-
+        self.heat_capacity = heat_capacity

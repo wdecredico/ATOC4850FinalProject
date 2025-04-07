bad33e725be115d2a7127607decaa6cddcf9fb65
Winston DeCredico <winstondecredico@gmail.com>
2025-04-07 00:29:35 -0600

    made x any y diffusion into one class

--- equations.py
+++ equations.py
@@ -64,6 +64,65 @@ class GreybodyConservationOfEnergyEquation(DyDtEquation):
         # returns in units of kelvin/second
         return dTdt
 
+class Diffusion(DyDtEquation):
+
+    def __init__(self, diffusion_coefficient: float, grid: Grid):
+        self.diffusion_coefficient = diffusion_coefficient
+        self.grid = grid
+
+    def __call__(self, time: float, temperatures: np.ndarray) -> np.ndarray:
+        """Calculates the temperature change in units of [units here] for each cell midpoint of the inputted array.
+
+        Parameters
+        ----------
+        time: function does not depend on time, function only takes it for consistency, input a float of whatever
+        temperatures: A 2d numpy array of temperature at each grid cell. Must include boundary."""
+
+        def x_direction_diffusion(temperature: np.ndarray) -> np.ndarray:
+            """Calculates temperature diffusion along the x-axis.  Returns a 2d numpy array
+                of size (y_dimension, x_dimension) with values of diffusion (dT/dt) in units
+                of [units here] for each grid midpoint position."""
+
+            # temperature change across cells in the x direction
+            dt_dx = (temperature[1:-1, 1:] - temperature[1:-1, :-1]) / self.grid.x_segment_length
+
+            flux: np.ndarray = -1 * self.diffusion_coefficient * dt_dx
+
+            incoming_shortwave_radiation = (Constants.SOLAR_IRRADIANCE / 4) * (1 - Constants.EARTH_ALBEDO)
+            outgoing_longwave_radiation = (1 - Constants.EMISSIVITY / 2) * Constants.STEFAN_BOLTZMANN_CONSTANT * (
+                    temperature[1:-1, 1:-1] ** 4)
+            flux_derivative = (flux[:, 1:] - flux[:, :-1]) / self.grid.x_segment_length
+
+            x_direction_diffusion = ((incoming_shortwave_radiation - outgoing_longwave_radiation - flux_derivative) /
+                                     self.grid.heat_capacity)
+
+            return x_direction_diffusion
+
+        def y_direction_diffusion(temperature: np.ndarray) -> np.ndarray:
+            """Calculates temperature diffusion along the y-axis.  Returns a 2d numpy array
+            of size (y_dimension, x_dimension) with values of diffusion (dT/dt) in units
+            of [units here] for each grid midpoint position."""
+
+            # temperature change across cells in the y direction
+            dt_dy = (temperatures[1: , 1:-1] - temperatures[:-1, 1:-1]) / self.grid.y_segment_length
+
+            flux: np.ndarray = -1 * self.diffusion_coefficient * dt_dy
+
+            incoming_shortwave_radiation = (Constants.SOLAR_IRRADIANCE / 4) * (1 - Constants.EARTH_ALBEDO)
+            outgoing_longwave_radiation = ((1 - Constants.EMISSIVITY / 2) * Constants.STEFAN_BOLTZMANN_CONSTANT *
+                                           (temperatures[1:-1, 1:-1] ** 4))
+            flux_derivative = (flux[1:, :] - flux[:-1, :]) / self.grid.y_segment_length
+
+            x_direction_diffusion = ((incoming_shortwave_radiation - outgoing_longwave_radiation - flux_derivative) /
+                                     self.grid.heat_capacity)
+
+            return x_direction_diffusion
+
+        return x_direction_diffusion(temperatures) + y_direction_diffusion(temperatures)
+
+
+
+
 
 class XDirectionDiffusion(DyDtEquation):
 
@@ -84,7 +143,6 @@ class XDirectionDiffusion(DyDtEquation):
         outgoing_longwave_radiation = (1 - Constants.EMISSIVITY/2) * Constants.STEFAN_BOLTZMANN_CONSTANT * (temperature[1:-1, 1:-1]**4)
         x_direction_flux_term = (flux[:, 1:] - flux[:, :-1]) / self.grid.x_segment_length
 
-        x_direction_diffusion = np.zeros((self.grid.y_dimension, self.grid.x_dimension))
         x_direction_diffusion = (incoming_shortwave_radiation - outgoing_longwave_radiation - x_direction_flux_term) / self.grid.heat_capacity
 
         return x_direction_diffusion
